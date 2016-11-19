7309c63ea474c3706882c0fef3a3ee18f04d8b32
Gleisson Assis <gleissonassis@gleissons-mbp.lan>
2016-11-19 19:33:31 -0200

    Cópia inicial

---
+++ sa/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sa
+//
+//  Created by Gleisson Assis on 11/19/16.
+//  Copyright © 2016 Gleisson de Assis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
